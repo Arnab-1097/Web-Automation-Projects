95510173003a1016d331c328ca230011e8d6efde
Arnab-1097 <85272127+Arnab-1097@users.noreply.github.com>
2024-02-04 01:13:22 +0600

    Update sent message to searched contact.py

--- sent message to searched contact.py
+++ sent message to searched contact.py
@@ -7,7 +7,7 @@ driver.get("https://web.whatsapp.com/")
 time.sleep(10)
 thumbnail=driver.find_element_by_xpath("//div[@role='textbox']")
 thumbnail.click()
-thumbnail.send_keys("01970208542")
+thumbnail.send_keys("")
 thumbnail.send_keys(Keys.ENTER)
 #thumbnail=driver.find_element_by_xpath("//body/div[@id='app']/div[@class='_1ADa8 _3Nsgw app-wrapper-web font-fix os-win']/div[@class='_1XkO3 two']/div[@class='ldL67 _2i3T7 _191H_']/div[@id='side']/div[@id='pane-side']/div/div/div[@aria-label='Chat list']/div[11]/div[1]/div[1]/div[2]")
 
